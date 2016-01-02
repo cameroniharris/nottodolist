0736e7a5bef04885623f83cfd7bb5c447567379f
cameroniharris <cameroniharris1@gmail.com>
2016-01-02 23:16:37 +0000

    moved links from footer, added homepage, put limit on how many to do items, fixed up the design

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,20 +1,49 @@
 class ListsController < ApplicationController
     
-    before_action :authenticate_user!, :only => [:new, :create]
+    before_action :authenticate_user!, :only => [:new, :create, :edit]
     def index
-        @lists = List.all
+        if user_signed_in?
+            @lists = current_user.lists
+        else
+            redirect_to new_user_session_path 
+            
+        end
         
     end
     
     def new 
         @list = List.new
-        
     end 
     
 
   def create
-    current_user.lists.create(list_params)
-    redirect_to root_path
+    if current_user.lists.count < 4 
+        current_user.lists.create(list_params)
+        redirect_to lists_path
+    else 
+        redirect_to root_url, :notice => "message here"
+    end
+  end
+  
+  def show
+      @list = List.find(params[:id])
+  end 
+  
+  def edit
+      @list = List.find(params[:id])
+
+  end
+  
+  def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      redirect_to root_path
+  end
+  
+  def update
+      @list = List.find(params[:id])
+      @list.update_attributes(list_params)
+      redirect_to root_path
   end
 
   private
